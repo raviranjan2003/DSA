8dd88f7a73994d88bcfb04f0ee41c76b8b0ef8b0
Ravi Ranjan <2140131@sliet.ac.in>
2023-11-21 17:40:30 +0530

    Bubble sort modified for best case

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -1,14 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// void bubbleSort(int arr[], int n){
+//     for(int i = n-1; i>=0; i--){
+//         for(int j=0;j<=i-1;j++){
+//             if(arr[j]>arr[j+1]){
+//                 int temp = arr[j];
+//                 arr[j]=arr[j+1];
+//                 arr[j+1]=temp;
+//             }
+//         }
+//     }
+// }
 void bubbleSort(int arr[], int n){
     for(int i = n-1; i>=0; i--){
+        int needSwap = 0;
         for(int j=0;j<=i-1;j++){
             if(arr[j]>arr[j+1]){
                 int temp = arr[j];
                 arr[j]=arr[j+1];
                 arr[j+1]=temp;
+                needSwap = 1;
             }
+        }
+        if(needSwap==0){
+            break;
         }
     }
 }
